acb1d18d9f1cc2475ad2090a0d8fa7a7083750de
NikiThefrog <pietje1976puk@gmail.com>
2025-10-15 13:44:38 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,6 @@
-let dialogue
-function dialogue ()
+let response1 = document.getElementById("response1")
+let response2 = document.getElementById("response2")
+let response3 = document.getElementById("response3")
+function dialogue (response1, response2, response3) {
+    
+}
