9067d32cbb539b64aecfca8566358516e2de3c4b
NikiThefrog <pietje1976puk@gmail.com>
2025-10-15 14:16:55 +0200

    javascript and fixed image

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,7 @@ let response1 = document.getElementById("response1")
 let response2 = document.getElementById("response2")
 let response3 = document.getElementById("response3")
 let dialogue = document.getElementById ("dialogue")
-response1Button.addeventlistener("click", function(response1, dialogue){
-    dialogue.textContent = "Wuh huh"
+response1.addeventlistener("click", function() {
+    dialogue.textContent = "Wuh huh";
+    console.log("wuh huh");
 });
