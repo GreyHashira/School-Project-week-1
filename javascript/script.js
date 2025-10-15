5505e0fb9a89d846414c6fc629db2b928794bb61
NikiThefrog <pietje1976puk@gmail.com>
2025-10-15 14:41:41 +0200

    99% of javascript copers quit right before the code works

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,15 @@ let response1 = document.getElementById("response1")
 let response2 = document.getElementById("response2")
 let response3 = document.getElementById("response3")
 let dialogue = document.getElementById("dialogue")
+
 response1.addeventlistener("click", function() {
     dialogue.textContent = "Wuh huh";
 });
+
 response2.addeventlistener("click", function() {
     dialogue.textContent = "Wuh huh";
 });
+
 response3.addeventlistener("click", function() {
     dialogue.textContent = "Wuh huh";
 });
