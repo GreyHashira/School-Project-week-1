42fe054ec2be4bff5503e9d591311496280d6bc6
NikiThefrog <pietje1976puk@gmail.com>
2025-10-15 14:02:12 +0200

    javascript that i did in a monster energy and adhd fueled fever dream, it does not work but god does it look like it should

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 let response1 = document.getElementById("response1")
 let response2 = document.getElementById("response2")
 let response3 = document.getElementById("response3")
-function dialogue (response1, response2, response3) {
-    
-}
+let dialogue = document.getElementById ("dialogue")
+response1Button.addeventlistener("click", function(response1, dialogue){
+    dialogue.textContent = "Wuh huh"
+});
